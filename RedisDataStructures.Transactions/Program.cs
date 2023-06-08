a334372b03b2ca18578b022756102653dba7d47e
Reza Heidari <m.heidari988@gmail.com>
2023-06-08 19:25:49 +0300

    Update Program.cs

--- RedisDataStructures.Transactions/Program.cs
+++ RedisDataStructures.Transactions/Program.cs
@@ -11,3 +11,28 @@ var config = new ConfigurationOptions
 using var redis = await ConnectionMultiplexer.ConnectAsync(config);
 var db = redis.GetDatabase();
 
+var transaction = db.CreateTransaction();
+
+_ = transaction.HashSetAsync("person:1", new HashEntry[]
+{
+    new("name", "John"),
+    new("age", 32),
+    new("postalCode", "223143"),
+});
+_ = transaction.SortedSetAddAsync("person:name:Steve", "person:2", 0);
+_ = transaction.SortedSetAddAsync("person:postal_code:32999", "person:2", 0);
+_ = transaction.SortedSetAddAsync("person:age", "person:2", 32);
+
+var success = await transaction.ExecuteAsync();
+
+Console.WriteLine($"Transaction Successful: {success}");
+
+
+// Adding conditional logic to a transaction
+
+transaction.AddCondition(Condition.HashEqual("person:1", "age", 32));
+_ = transaction.HashIncrementAsync("person:1", "age");
+_ = transaction.SortedSetIncrementAsync("person:age", "person:2", 1);
+
+success = await transaction.ExecuteAsync();
+Console.WriteLine($"Transaction Successful: {success}");
