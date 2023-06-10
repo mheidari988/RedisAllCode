5b281135d078e90fe4aceb39c36dd6e175e074c6
Reza Heidari <m.heidari988@gmail.com>
2023-06-10 20:31:06 +0300

    add session state project

---
+++ RedisAspNetCore.Sessions/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RedisAspNetCore.Sessions.Models;
+using System.Diagnostics;
+
+namespace RedisAspNetCore.Sessions.Controllers;
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        var fourtyTwo = 42;
+        HttpContext.Session.SetString("meaning of life", fourtyTwo.ToString());
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
